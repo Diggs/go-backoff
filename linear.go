328376e1e3223f8f8ffb659b78f50d8af7a47f28
Tyler Power <tylerpowerx@gmail.com>
2015-03-15 22:15:54 +1300

    run gofmt

--- linear.go
+++ linear.go
@@ -1,35 +1,35 @@
 package backoff
 
 import (
-  "time"
+	"time"
 )
 
 type Linear struct {
-  LastWait time.Duration
-  increment time.Duration
-  limit time.Duration
-  count int
+	LastWait  time.Duration
+	increment time.Duration
+	limit     time.Duration
+	count     int
 }
 
-func(lin *Linear) Reset() {
-  lin.count = 0
+func (lin *Linear) Reset() {
+	lin.count = 0
 }
 
-func(lin *Linear) getBackoff() time.Duration {
-  backoff := time.Duration(lin.count) * lin.increment
-  if lin.limit > 0 && backoff > lin.limit {
-    backoff = lin.limit
-  }
-  return backoff
+func (lin *Linear) getBackoff() time.Duration {
+	backoff := time.Duration(lin.count) * lin.increment
+	if lin.limit > 0 && backoff > lin.limit {
+		backoff = lin.limit
+	}
+	return backoff
 }
 
-func(lin *Linear) Backoff() {
-  backoff := lin.getBackoff()
-  time.Sleep(backoff)
-  lin.count++
-  lin.LastWait = backoff
+func (lin *Linear) Backoff() {
+	backoff := lin.getBackoff()
+	time.Sleep(backoff)
+	lin.count++
+	lin.LastWait = backoff
 }
 
 func NewLinear(increment time.Duration, limit time.Duration) *Linear {
-  return &Linear{increment:increment, limit:limit}
+	return &Linear{increment: increment, limit: limit}
 }
--- linear_test.go
+++ linear_test.go
@@ -1,26 +1,26 @@
 package backoff
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
-  "time"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
 )
 
 func TestLinearBackoff(t *testing.T) {
-  exp := NewLinear(1 * time.Millisecond, 0)
-  for i := 0; i < 5; i++ {
-    start := time.Now()
-    exp.Backoff()
-    end := time.Now()
-    assert.InDelta(t, end.UnixNano(), start.UnixNano(), (TIMING_JITTER_COMP * float64(i)) * float64(time.Millisecond))
-  }
+	exp := NewLinear(1*time.Millisecond, 0)
+	for i := 0; i < 5; i++ {
+		start := time.Now()
+		exp.Backoff()
+		end := time.Now()
+		assert.InDelta(t, end.UnixNano(), start.UnixNano(), (TIMING_JITTER_COMP*float64(i))*float64(time.Millisecond))
+	}
 }
 
 func TestLinearLimit(t *testing.T) {
-  exp := NewLinear(250 * time.Millisecond, time.Second)
-  for i := 0; i < 6; i++ {
-    exp.Backoff()
-  }
-  assert.Equal(t, 6, exp.count)
-  assert.Equal(t, time.Second, exp.getBackoff())
+	exp := NewLinear(250*time.Millisecond, time.Second)
+	for i := 0; i < 6; i++ {
+		exp.Backoff()
+	}
+	assert.Equal(t, 6, exp.count)
+	assert.Equal(t, time.Second, exp.getBackoff())
 }
