8ad219e6dff7e336618c9cfe9e391a1e45afe831
Tyler Power <tylerpowerx@gmail.com>
2015-03-16 14:57:16 +1300

    Add comments for public properties. Track NextDuration so it can be used externally (e.g. for logging) even when using a jittery algorithm.

--- backoff.go
+++ backoff.go
@@ -7,46 +7,65 @@ import (
 	"time"
 )
 
+// BackoffStrategy can be implemented to provide different backoff algorithms.
 type BackoffStrategy interface {
+	// GetBackoffDuration calculates the next time.Duration that the current thread will sleep for when backing off.
+	// It receives the current backoff count, the initial backoff duration and the last back off duration.
 	GetBackoffDuration(int, time.Duration, time.Duration) time.Duration
 }
 
+// Backoff tracks the generic state of the configured back off strategy.
 type Backoff struct {
-	LastBackoff time.Duration
-	start       time.Duration
-	limit       time.Duration
-	count       int
-	strategy    BackoffStrategy
+	// LastDuration contains the duration that was previously waited, or 0 if no backoff has occurred yet.
+	LastDuration time.Duration
+	// NextDuration contains the duration that will be waited on the next call to Backoff().
+	NextDuration time.Duration
+	start        time.Duration
+	limit        time.Duration
+	count        int
+	strategy     BackoffStrategy
 }
 
+// Creates a new backoff using the specified BackoffStrategy, start duration and limit.
+func NewBackoff(strategy BackoffStrategy, start time.Duration, limit time.Duration) *Backoff {
+	backoff := Backoff{strategy: strategy, start: start, limit: limit}
+	backoff.Reset()
+	return &backoff
+}
+
+// Reset sets the Backoff to its initial conditions ready to start over.
 func (exp *Backoff) Reset() {
 	exp.count = 0
+	exp.LastDuration = 0
+	exp.NextDuration = exp.getNextDuration()
 }
 
-func (exp *Backoff) GetBackoffDuration() time.Duration {
-	backoff := exp.strategy.GetBackoffDuration(exp.count, exp.start, exp.LastBackoff)
+// Backoff causes the current thread/routine to sleep for NextDuration.
+func (exp *Backoff) Backoff() {
+	time.Sleep(exp.NextDuration)
+	exp.count++
+	exp.LastDuration = exp.NextDuration
+	exp.NextDuration = exp.getNextDuration()
+}
+
+func (exp *Backoff) getNextDuration() time.Duration {
+	backoff := exp.strategy.GetBackoffDuration(exp.count, exp.start, exp.LastDuration)
 	if exp.limit > 0 && backoff > exp.limit {
 		backoff = exp.limit
 	}
 	return backoff
 }
 
-func (exp *Backoff) Backoff() {
-	backoff := exp.GetBackoffDuration()
-	time.Sleep(backoff)
-	exp.count++
-	exp.LastBackoff = backoff
-}
-
 type exponential struct{}
 
 func (exponential) GetBackoffDuration(backoffCount int, start time.Duration, lastBackoff time.Duration) time.Duration {
-	period := int(math.Pow(2, float64(backoffCount)))
+	period := int64(math.Pow(2, float64(backoffCount)))
 	return time.Duration(period) * start
 }
 
+// NewExponential creates a new backoff using the exponential backoff algorithm.
 func NewExponential(start time.Duration, limit time.Duration) *Backoff {
-	return &Backoff{strategy: exponential{}, start: start, limit: limit}
+	return NewBackoff(exponential{}, start, limit)
 }
 
 type exponentialFullJitter struct{}
@@ -57,8 +76,9 @@ func (exponentialFullJitter) GetBackoffDuration(backoffCount int, start time.Dur
 	return time.Duration(jitter.Int64())
 }
 
+// NewExponentialFullJitter creates a new backoff using the exponential with full jitter backoff algorithm.
 func NewExponentialFullJitter(start time.Duration, limit time.Duration) *Backoff {
-	return &Backoff{strategy: exponentialFullJitter{}, start: start, limit: limit}
+	return NewBackoff(exponentialFullJitter{}, start, limit)
 }
 
 type linear struct{}
@@ -67,6 +87,7 @@ func (linear) GetBackoffDuration(backoffCount int, start time.Duration, lastBack
 	return time.Duration(backoffCount) * start
 }
 
+// NewLinear creates a new backoff using the linear backoff algorithm.
 func NewLinear(start time.Duration, limit time.Duration) *Backoff {
-	return &Backoff{strategy: linear{}, start: start, limit: limit}
+	return NewBackoff(linear{}, start, limit)
 }
--- backoff_test.go
+++ backoff_test.go
@@ -10,33 +10,33 @@ import (
 func TestExponentialBackoff(t *testing.T) {
 	exp := NewExponential(time.Millisecond, 0)
 	for i := 0; i < 5; i++ {
-		assert.Equal(t, int64(float64(time.Millisecond)*math.Pow(2, float64(i))), exp.GetBackoffDuration())
+		assert.Equal(t, int64(float64(time.Millisecond)*math.Pow(2, float64(i))), exp.NextDuration)
 		exp.Backoff()
 	}
 }
 
 func TestExponentialLimit(t *testing.T) {
-	exp := NewExponential(250*time.Millisecond, time.Second)
+	exp := NewExponential(time.Millisecond, 4*time.Millisecond)
 	for i := 0; i < 5; i++ {
 		exp.Backoff()
 	}
 	assert.Equal(t, 5, exp.count)
-	assert.Equal(t, time.Second, exp.GetBackoffDuration())
+	assert.Equal(t, 4*time.Millisecond, exp.NextDuration)
 }
 
 func TestLinearBackoff(t *testing.T) {
 	lin := NewLinear(time.Millisecond, 0)
 	for i := 0; i < 5; i++ {
-		assert.Equal(t, int64(float64(time.Millisecond)*float64(i)), lin.GetBackoffDuration())
+		assert.Equal(t, int64(float64(time.Millisecond)*float64(i)), lin.NextDuration)
 		lin.Backoff()
 	}
 }
 
 func TestLinearLimit(t *testing.T) {
-	lin := NewLinear(250*time.Millisecond, time.Second)
+	lin := NewLinear(time.Millisecond, 4*time.Millisecond)
 	for i := 0; i < 5; i++ {
 		lin.Backoff()
 	}
 	assert.Equal(t, 5, lin.count)
-	assert.Equal(t, time.Second, lin.GetBackoffDuration())
+	assert.Equal(t, 4*time.Millisecond, lin.NextDuration)
 }
